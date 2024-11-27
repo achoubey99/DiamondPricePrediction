caaf9586061b1e393de88cb0f280cf1b106dfeca
achoubey99 <achoubey99@gmail.com>
2024-11-27 22:07:50 +0530

    updated the code to show the output on the same page

--- app.py
+++ app.py
@@ -37,47 +37,28 @@ def getData():
         obj_cols = ['cut', 'color', 'clarity']
         num_cols = ['depth', 'table', 'x', 'y', 'z', 'carat']
 
-        # Validate input
-        for col in obj_cols + num_cols:
-            if col not in input_df.columns:
-                return jsonify({"error": f"Missing column: {col}"}), 400
-
         # Preprocess categorical and numerical data
         categorical_data = [form_data[x] for x in obj_cols]
 
-        print('categorical data',categorical_data)
         #input_df[num_cols] = input_df[num_cols].apply(pd.to_numeric, errors='coerce')  # Handle non-numeric values
         numerical_data = [float(form_data[x]) for x in num_cols]
-        
-        print('numerical data',numerical_data)
 
         categorical_data.extend(numerical_data)
 
-        print('input list', categorical_data)
-
         # Combine processed columns
         input_data = [categorical_data]
-        print('input array', input_data)
+        
 
         input_df = pd.DataFrame(input_data, columns = obj_cols + num_cols)
         
-        if len(input_data[0]) != 9:
-            raise ValueError("Input data must have 9 features.", input_df)
-        print(input_df)
         processed_input = loaded_transformed_col.transform(input_df)
-
-        print('processing done')
 
         # Making prediction prediction
         prediction = model.predict(processed_input)
-        if prediction.size == 0:
-            raise ValueError("Prediction is empty")
-
-        print('prediction done')
         
         # Return prediction
         result = "Predicted Price " + "= " + "$" + f"{round(float(prediction[0]),2)}"
-        return result 
+        return render_template('index.html', prediction_text = result )
         
 
     except Exception as e:
